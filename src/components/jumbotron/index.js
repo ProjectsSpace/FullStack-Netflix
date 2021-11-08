ea184066a7bb12d34ccbb9925cb21b70ce1bb274
Mocarram Hosan <mocarramhosan@gmail.com>
2021-11-09 03:42:19 +0600

    Normal function to arrow function

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -9,37 +9,33 @@ import {
 } from "./styles/jumbotron";
 
 // The main Jumbotron element that hold single Jumbotrons
-const Jumbotron = ({ children, direction = "row", ...restProps }) => {
-  return (
-    <Item {...restProps}>
-      <Inner direction={direction}>{children}</Inner>
-    </Item>
-  );
-};
+const Jumbotron = ({ children, direction = "row", ...restProps }) => (
+  <Item {...restProps}>
+    <Inner direction={direction}>{children}</Inner>
+  </Item>
+);
 
 // Container that holds the whole Jumbotron
-Jumbotron.Container = function JumbotronContainer({ children, ...restProps }) {
-  return <Container {...restProps}>{children}</Container>;
-};
+Jumbotron.Container = ({ children, ...restProps }) => (
+  <Container {...restProps}>{children}</Container>
+);
 
 // Pane holds the certain groups of elements
-Jumbotron.Pane = function JumbotronPane({ children, ...restProps }) {
-  return <Pane {...restProps}>{children}</Pane>;
-};
+Jumbotron.Pane = ({ children, ...restProps }) => (
+  <Pane {...restProps}>{children}</Pane>
+);
 
 // Title element for the Jumbotron
-Jumbotron.Title = function JumbotronTitle({ children, ...restProps }) {
-  return <Title {...restProps}>{children}</Title>;
-};
+Jumbotron.Title = ({ children, ...restProps }) => (
+  <Title {...restProps}>{children}</Title>
+);
 
 // Subtitle element for the Jumbotron
-Jumbotron.SubTitle = function JumbotronSubTitle({ children, ...restProps }) {
-  return <SubTitle {...restProps}>{children}</SubTitle>;
-};
+Jumbotron.SubTitle = ({ children, ...restProps }) => (
+  <SubTitle {...restProps}>{children}</SubTitle>
+);
 
 // Image element for the Jumbotron
-Jumbotron.Image = function JumbotronImage({ children, ...restProps }) {
-  return <Image {...restProps} />;
-};
+Jumbotron.Image = ({ children, ...restProps }) => <Image {...restProps} />;
 
 export default Jumbotron;
